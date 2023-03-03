5405dd2485a69b9ba6130893bdda2c0f07307a26
erden pınarözü <erdener052@hotmail.com>
2023-03-03 05:07:18 +0300

    .

--- src/views/GameInfo.js
+++ src/views/GameInfo.js
@@ -1,76 +1,62 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setInfo } from "../store/reducers/gameInfoActions";
 import "./GameInfo.css"
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/free-mode";
+import { Autoplay, Pagination, Navigation, FreeMode } from "swiper";
+
 const GameInfo = () =>{
-    const game = {
-        id: 452,
-        title: "Call Of Duty: Warzone",
-        thumbnail: "https://www.mmobomb.com/g/452/thumbnail.jpg",
-        status: "Live",
-        short_description: "One of the most popular FPSes in the world is now a free-to-play battle royale: Call of Duty: Warzone. Strive to become the last squad standing in Battle Royale mode, where 150 players loot and shoot their way through an ever-shrinking battlefield; or complete contracts and try to escape with the most cash in Plunder mode.",
-        description: "<p style=\"text-align: justify;\">One of the most popular FPSes in the world is now a free-to-play battle royale: Call of Duty: Warzone. Strive to become the last squad standing in Battle Royale mode, where 150 players loot and shoot their way through an ever-shrinking battlefield; or complete contracts and try to escape with the most cash in Plunder mode. Money can also buy you weapons and perks from stations throughout the map, but if you're taken out, you'll lose everything you've accumulated!</p>\r\n<p style=\"text-align: justify;\">Another innovation in CoD: Warzone is the gulag. If you're gunned down during a Battle Royale match, you'll be sent to the gulag with a chance to win a 1v1 for the right to return to the battlefield. Also, progression is shared between Warzone and Call of Duty: Modern Warfare. If you don't own CoD:MW, you'll keep your progression from Warzone if you buy it later.</p>\r\n",
-        game_url: "https://www.mmobomb.com/open/call-of-duty-warzone",
-        genre: "Shooter",
-        platform: "Windows",
-        publisher: "Activision",
-        developer: "Infinity Ward",
-        release_date: "2020-03-10",
-        profile_url: "https://www.mmobomb.com/call-of-duty-warzone",
-        minimum_system_requirements: {
-        os: "Windows 7 64-Bit (SP1) or Windows 10 64-Bit",
-        processor: "Intel Core i3-4340 or AMD FX-6300",
-        memory: "8GB RAM",
-        graphics: "NVIDIA GeForce GTX 670 / GeForce GTX 1650 or Radeon HD 7950",
-        storage: "175GB HD space"
-        },
-        screenshots: [
-        {
-        id: 1124,
-        image: "https://www.mmobomb.com/g/452/Call-of-Duty-Warzone-1.jpg"
-        },
-        {
-        id: 1125,
-        image: "https://www.mmobomb.com/g/452/Call-of-Duty-Warzone-2.jpg"
-        },
-        {
-        id: 1126,
-        image: "https://www.mmobomb.com/g/452/Call-of-Duty-Warzone-3.jpg"
-        },
-        {
-        id: 1127,
-        image: "https://www.mmobomb.com/g/452/Call-of-Duty-Warzone-4.jpg"
-        }
-        ]
-    }
     const infoState=useSelector(state=>state.gameInfo)
-    const [gamex, setGame] = useState([]);
+    const [control, setControl] = useState(false);
+    const [game, setGame] = useState([]);
     const dispatch=useDispatch()
-    const options = {
-        method: 'GET',
-        url: 'https://mmo-games.p.rapidapi.com/game',
-        params: {id: infoState.id},
-        headers: {
-          'X-RapidAPI-Key': '30fa44aac7msha71c0e6a85a26abp10736fjsn78683a7be182',
-          'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+
+    useEffect(() => {
+        if (infoState.id !== 0) {
+           const options = {
+                method: 'GET',
+                url: 'https://mmo-games.p.rapidapi.com/game',
+                params: {id: infoState.id},
+                headers: {
+                'X-RapidAPI-Key': '30fa44aac7msha71c0e6a85a26abp10736fjsn78683a7be182',
+                'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+                }
+            };
+            
+          axios(options).then(response => {
+            setGame(response.data)
+            setControl(true)
+            console.log(typeof(infoState.id))
+            console.log(response.data)
+          }).catch(function (error) {
+            console.error(error);
+          });
+        } else {
+          console.log("hatalı id "+ infoState.id)  
         }
-      };
-      
-      axios(options).then(response => {
-        setGame(response.data)
-      }).catch(function (error) {
-        console.error(error);
-      });
+        
+    }, [infoState]);
+    
         // <div className="testDeneme">
         // {gameNews.length>0 && (<td dangerouslySetInnerHTML={{__html: gameNews[12].article_content}} />)}
         // </div>
   return (
     <>
-    {infoState.show&&(<div className="gameInfoBody" onClick={()=>{
+    {infoState.show && control &&(<div className="gameInfoBody">
+        <div className="overlay" onClick={()=>{
+        setControl(false)
         dispatch(setInfo(false,0))
     }}>
-        <div className="gameInfoContainer">
+        </div>
+        <div className="gameInfoContainer" onClick={()=>{
+            console.log("clıck oldu");
+        }}>
             <div className="gameInfoTop">
                 <div className="gameInfoAbout">
                     <div >
@@ -88,24 +74,52 @@ const GameInfo = () =>{
                             developer : {game.developer}
                         </div>
                     </div>
-                    <div className="gameInfoOS">
+                    {(game.minimum_system_requirements!==undefined)&&(<div className="gameInfoOS">
                         <h3 style={{margin:"1em 0.5em 0em"}}>Minimum System Requirements</h3>
                         <ul style={{listStyleType:"none",padding:"0 1em 0"}}>
-                            <li>os : {game.minimum_system_requirements.os}</li>
-                            <li>processor : {game.minimum_system_requirements.processor}</li>
-                            <li>memory : {game.minimum_system_requirements.memory}</li>
-                            <li>graphics : {game.minimum_system_requirements.graphics}</li>
-                            <li>storage : {game.minimum_system_requirements.storage}</li>
+                            {(game.minimum_system_requirements.os!==undefined)&&(<li>os : {game.minimum_system_requirements.os}</li>)}
+                            {(game.minimum_system_requirements.processor!==undefined)&&(<li>processor : {game.minimum_system_requirements.processor}</li>)}
+                           {(game.minimum_system_requirements.memory!==undefined)&&( <li>memory : {game.minimum_system_requirements.memory}</li>)}
+                            {(game.minimum_system_requirements.graphics!==undefined)&&(<li>graphics : {game.minimum_system_requirements.graphics}</li>)}
+                            {(game.minimum_system_requirements.storage!==undefined)&&(<li>storage : {game.minimum_system_requirements.storage}</li>)}
                         </ul>
-                    </div>
+                    </div>)}
                 </div>
                 <div className="gameInfoScreenshots">
-                    <img className="gameInfoScreenshot" src={game.screenshots[0].image}/>
+                    <Swiper
+                        freeMode={true}
+                        loop={true}
+                        slidesPerView={1}
+                        spaceBetween={30}
+                        centeredSlides={true}
+                        autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                        }}
+                        pagination={{
+                        clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Autoplay, Pagination, Navigation, FreeMode]}
+                        className="mySwiper"
+                    >
+                        {game.screenshots.length>0 && (game.screenshots.map((item)=>{
+                            return(
+                                <SwiperSlide>
+                                    <div className="screenshotContainer">
+                                        <img className="gameInfoScreenshot" src={item.image}/>                                        
+                                    </div>
+                                    </SwiperSlide>
+                            )
+                        }))}
+                    </Swiper>
                 </div>
             </div>
-
             <div className="gameInfoDescription">
                 <td dangerouslySetInnerHTML={{__html: game.description}} />
+            </div>
+            <div style={{margin:"0.5em 0 1em",display:"flex",justifyContent:"center",fontSize:"1.2em"}}>
+                <a href={game.game_url} style={{textDecoration:"none", color:"black", border:"3px black solid",borderRadius:"20px",padding:"0 1em" }} target="_blank">Go to Game</a>
             </div>
         </div>
     </div>)}
